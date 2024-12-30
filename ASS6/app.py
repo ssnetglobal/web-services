94668273c4d0f6a66152b1281ddc818c6fd641d0
ssnetglobal <stephenobiya3@gmail.com>
2024-12-31 01:31:33 +0300

    fix ci-cd

--- ASS6/app.py
+++ ASS6/app.py
@@ -1,1 +1,11 @@
-hello
+def greet(name: str) -> str:
+    """
+    Returns a greeting message for the given name.
+
+    Args:
+        name (str): The name of the person to greet.
+
+    Returns:
+        str: A greeting message.
+    """
+    return f"Hello, {name}!"
