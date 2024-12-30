6744bf58687e3a7eb95f23c98e6b13a2bd3ce151
ssnetglobal <stephenobiya3@gmail.com>
2024-12-31 01:35:56 +0300

    fix ci-cd

--- ASS6/test_sample.py
+++ ASS6/test_sample.py
@@ -1,7 +1,7 @@
-def add(a, b):
-    return a + b
+import pytest
+from app import greet
 
-def test_add():
-    assert add(2, 3) == 5
-    assert add(-1, 1) == 0
-    assert add(0, 0) == 0
+def test_greet():
+    assert greet("Alice") == "Hello, Alice!"
+    assert greet("Bob") == "Hello, Bob!"
+    assert greet("") == "Hello, !"
